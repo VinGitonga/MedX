1ea8eac6f555733aa5cab451a0080bb001eaa94f
VinGitonga <gitongavincent64@gmail.com>
2022-01-16 21:10:17 +0300

    added landing page

---
+++ firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+import { getStorage } from 'firebase/storage'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDoLDij2Y2OPZPLgDQfHV1AADHFI8jISxg",
+    authDomain: "medx-pro-34a1c.firebaseapp.com",
+    projectId: "medx-pro-34a1c",
+    storageBucket: "medx-pro-34a1c.appspot.com",
+    messagingSenderId: "1021714847630",
+    appId: "1:1021714847630:web:bbc89c77b9632502f336df"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+const db = getFirestore(app)
+const storage = getStorage()
+const auth = getAuth(app)
+
+export { app, db, storage, auth }
