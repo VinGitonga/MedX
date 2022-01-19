bd08fa3f252ff6c290aa4cf11ba6c25782e6be5e
VinGitonga <gitongavincent64@gmail.com>
2022-01-19 20:10:03 +0300

    created shared components

---
+++ src/components/common/Spinning.js
@@ -0,0 +1,15 @@
+import { Spinner } from '@chakra-ui/react'
+
+const Spinning = () => (
+    <div style={{ position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)", opacity: 0.3 }}>
+        <Spinner
+            thickness='4px'
+            speed='0.65s'
+            emptyColor='gray.200'
+            color='blue.500'
+            size='xl'
+        />
+    </div>
+)
+
+export default Spinning
